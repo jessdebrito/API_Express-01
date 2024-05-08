3147c1d7bde44344218da77e1cf00d6b27348c0b
jessdebrito <debrito.herself@gmail.com>
2024-05-08 20:28:33 -0300

    App - Course Router

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express, { json } from "express";
+import { courseRouter } from "./routes/course.routes";
 export const app = express();
 
 app.use(json());
+
+app.use("/courses", courseRouter);
